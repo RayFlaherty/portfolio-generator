d84a03797f282b5c0cd0b3fe6008372060ce9c33
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2021-11-29 23:01:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const profileDataArgs= process.argv.slice(2,process.argv.length);
-console.log(profileDataArgs);
 
-
-const printProfileData= (profileDataArr) => {
-  console.log(profileDataArr);
+const printProfileData= profileDataArr => {
+  for (let i=0; i < profileDataArr.Length; i++) {
+    console.log(profileDateArr{i});
+  }
 };
 
 printProfileData (profileDataArgs);
