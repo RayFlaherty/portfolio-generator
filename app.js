636b61b17d296ad411a4e8785ba62b8155f3d49d
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2021-11-30 22:22:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const profileDataArgs= process.argv.slice(2,process.argv.length);
+const generatePage= () => 'Name : Ray, Github: RayFlaherty';
 
+console.log(generatePage());
 //const printProfileData = profileDataArr => {
   //This...
   //for (let i =0; i < profileDataArr.Length; i += 1) {
