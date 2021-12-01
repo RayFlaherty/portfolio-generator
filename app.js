d396a91fd64cb0ad54072c3dd60be8e6b5b1a523
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2021-11-30 22:18:42 -0600

    commenting out js
    prepping for lesson 2

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const profileDataArgs= process.argv.slice(2,process.argv.length);
 
-const printProfileData = profileDataArr => {
+//const printProfileData = profileDataArr => {
   //This...
-  for (let i =0; i < profileDataArr.Length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+  //for (let i =0; i < profileDataArr.Length; i += 1) {
+    //console.log(profileDataArr[i]);
+  //}
   
-  console.log('===================');
+  //console.log('===================');
   
   //Is the same as this ..
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+  //profileDataArr.forEach(profileItem => console.log(profileItem));
     
  
-};
+//};
 
-printProfileData (profileDataArgs);
+//printProfileData (profileDataArgs);
