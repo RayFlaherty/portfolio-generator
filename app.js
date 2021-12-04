98e82d564dee9d64d6a55dc04bdcac8cfa69e6f4
RayFlaherty <raymondf.flaherty@gmail.com>
2021-12-04 12:42:36 -0600

    udating user input

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
-const fs = require('fs');
+const inquirer= require ('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
 
-const generatePage = (name, github) => {
-  return `
-   <!DOCTYPE html>
-   <html lang="en">
-   <head>
-    <meta charset="UTRF-8">
-    <meta name = "viewport" content ="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-   </head>
-   
-   <body>
-     <h1>${name}</h1>
-     <h2><a href="https://github.com/${github}">Github</a></h2>
-   </body>
-   </html>
-  `; 
-};
+    }
+  ])
 
-fs.writeFile('index.html', generatePage(name, github),err=> {
-  if (err) throw err;
+  .then(answers => console.log(answers));
+
   
-  console.log('Portfolio complete! Check out index.html to see the output!')
-});
+
+//const fs = require('fs');
+
+//const profileDataArgs = process.argv.slice(2);
+
+//const [name, github] = profileDataArgs;
+
+//const generatePage = (name, github) => {
+  //return `
+   //<!DOCTYPE html>
+   //<html lang="en">
+   //<head>
+    //<meta charset="UTRF-8">
+    //<meta name = "viewport" content ="width=device-width, initial-scale=1.0">
+    //<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //<title>Portfolio Demo</title>
+   //</head>
+   
+   //<body>
+    // <h1>${name}</h1>
+    // <h2><a href="https://github.com/${github}">Github</a></h2>
+   //</body>
+   //</html>
+  //`; 
+//};
+
+//fs  .writeFile('index.html', generatePage(name, github),err=> {
+  //if (err) throw err;
+  
+  //console.log('Portfolio complete! Check out index.html to see the output!')
+//});
 
 
 //const printProfileData = profileDataArr => {
